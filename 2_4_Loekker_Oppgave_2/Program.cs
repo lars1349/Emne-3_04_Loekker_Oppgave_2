22bc23b1fc69788eba00b2d331d83f96322ef382
larsmostue <mostuelars@gmail.com>
2024-11-27 10:44:45 +0100

    Add project files.

---
+++ 2_4_Loekker_Oppgave_2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _2_4_Loekker_Oppgave_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listOfWords = {"D", "e", "t", "t", "e", "", "e", "r", "", "e", "n", "", "t", "e", "s", "t"};
+        foreach (string theSentence in listOfWords)
+            {
+                Console.WriteLine(theSentence);
+            }
+        }
+    }
+}
